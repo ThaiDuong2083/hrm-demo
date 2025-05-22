8a49108bf18e038f29b1c8be2f10d9f356e107b1
Thai Duong <leduong2083@gmail.com>
2025-05-23 00:34:08 +0700

    delete ConvertInRewardPolicy.java, ConvertInRewardPolicyApplicableTarget.java, ConvertInRewardPolicyLine.java

--- src/main/java/com/example/apus_hrm_demo/mapper/reward_policy/ConvertInRewardPolicy.java
+++
@@ -1,5 +0,0 @@
-package com.example.apus_hrm_demo.mapper.reward_policy;
-
-public class ConvertInRewardPolicy {
-
-}
--- src/main/java/com/example/apus_hrm_demo/mapper/reward_policy_line/ConvertInRewardPolicyLine.java
+++
@@ -1,5 +0,0 @@
-package com.example.apus_hrm_demo.mapper.reward_policy_line;
-
-public class ConvertInRewardPolicyLine {
-
-}
